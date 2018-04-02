8562e34f79ead3c347f3571f91f5e859ef160dc4
Alex Dean <alex@zanconsulting.com>
2018-04-02 09:14:03 -0600

    ExtJsAwareBrowserTrait - implemented answerMessageBox

--- ExtJs/ExtJsAwareBrowserTrait.php
+++ ExtJs/ExtJsAwareBrowserTrait.php
@@ -3,6 +3,8 @@
 
 namespace Zan\BrowserAutomationBundle\ExtJs;
 
+use Behat\Mink\Element\NodeElement;
+
 
 /**
  * Adds helper methods for working with ExtJs applications
@@ -75,6 +77,46 @@ trait ExtJsAwareBrowserTrait
             $findComponentJs,
             $setValue
         ));
+    }
+
+    /**
+     * Responds to the current active message box with $answer
+     *
+     * @param $answer string Exact text of button. ex: "Yes"
+     * @throws \Exception
+     * @throws \WebDriver\Exception\Timeout
+     */
+    public function answerMessageBox($answer)
+    {
+        $buttonXpath = sprintf("//*[text()='%s']", $answer);
+
+        /*
+         * This cannot use a normal click($answer) call due to the following error:
+         *
+         * WebDriver\Exception\UnknownError: unknown error: Element is not clickable at point (918, 538). Other element would receive the click
+         */
+        $this->waitForFunction(function() use ($buttonXpath) {
+            // Wait for a message box to appear
+            $inElement = $this->browserSession->getPage()->find('css', '.x-message-box');;
+            if (!$inElement) return false;
+
+            $elements = $inElement->findAll('xpath', $buttonXpath);
+
+            // Nothing found
+            if (!$elements) return false;
+
+            if (count($elements) > 1) {
+                throw new \InvalidArgumentException(sprintf('Text "%s" matched multiple elements', $buttonXpath));
+            }
+
+            /** @var NodeElement $element */
+            // Must click on the parent element and not the element with the text
+            $element = $elements[0]->getParent();
+            $element->click();
+
+            // Found and clicked element, return true
+            return true;
+        }, $this->getJavascriptExecutionTimeout());
     }
 
     protected function resolveSetValueToEscapedValue($query, $value)
